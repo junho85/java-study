d848b91850b4339c8d1b04b003279e3a815c2fa7
june.kim <june.kim@daumkakao.com>
2016-03-17 00:43:31 +0900

    add javamail sample

---
+++ src/main/java/Mail.java
@@ -0,0 +1,70 @@
+import org.apache.commons.cli.*;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Mail {
+    public static void main(String[] args) throws MessagingException, ParseException {
+        // --host=mail.test.com --port=25 --from=abc@test.com --to=test@test.com
+        CommandLineParser parser = new DefaultParser();
+
+        Options options = new Options();
+        options.addOption(Option.builder().longOpt("host")
+                .desc("smtp server host")
+                .hasArg()
+                .build());
+
+        options.addOption(Option.builder().longOpt("port")
+                .desc("smtp server port")
+                .hasArg()
+                .build());
+
+        options.addOption(Option.builder().longOpt("from")
+                .desc("from address")
+                .hasArg()
+                .build());
+
+        options.addOption(Option.builder().longOpt("to")
+                .desc("to address")
+                .hasArg()
+                .build());
+
+        CommandLine line = parser.parse(options, args);
+
+        String host = line.getOptionValue("host");
+        String port = line.getOptionValue("port");
+        String from = line.getOptionValue("from");
+        String to = line.getOptionValue("to");
+
+        String subject = "test subject";
+        String body = "test message";
+
+        Properties props = System.getProperties();
+        props.put("mail.smtp.starttls.enable", true);
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.ssl.protocols", "SSLv2Hello SSLv3");
+
+        Session session = Session.getDefaultInstance(props, null);
+        session.setDebug(true);
+
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from));
+
+        InternetAddress toAddress = new InternetAddress(to);
+
+        message.addRecipient(Message.RecipientType.TO, toAddress);
+
+        message.setSubject(subject);
+        message.setText(body);
+
+        Transport transport = session.getTransport("smtp");
+
+        transport.connect();
+
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
+}
