9e3a196bedc5143ea2cf6dfcbd3bb094d636fcaa
june.kim <june.kim@daumkakao.com>
2016-03-31 22:54:47 +0900

    fix

--- src/main/java/PunyCodeTest.java
+++ src/main/java/PunyCodeTest.java
@@ -2,6 +2,11 @@ import java.net.IDN;
 
 public class PunyCodeTest {
     public static void main(String[] args) {
-        System.out.println(IDN.toASCII("한글코딩"));
+        System.out.println(IDN.toASCII("한글코딩")); // xn--bj0bv9kgwxoqf
+        System.out.println(IDN.toASCII("daum")); // daum
+
+        System.out.println(IDN.toUnicode("daum")); // daum
+        System.out.println(IDN.toUnicode("한글코딩")); // 한글코딩
+        System.out.println(IDN.toUnicode("xn--bj0bv9kgwxoqf")); // 한글코딩
     }
 }
