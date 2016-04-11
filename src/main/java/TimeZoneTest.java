bf63a10664ea7a4395ee942c051905296d9d2bc4
june.kim <june.kim@daumkakao.com>
2016-04-11 22:20:22 +0900

    fix timezone test

--- src/main/java/TimeZoneTest.java
+++ src/main/java/TimeZoneTest.java
@@ -1,16 +1,18 @@
 import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class TimeZoneTest {
     public static void main(String[] args) {
-        System.out.println(LocalDateTime.now(ZoneId.of("GMT")));
-        System.out.println(LocalDateTime.now(ZoneId.of("UTC")));
-        System.out.println(Timestamp.valueOf(LocalDateTime.now(ZoneId.of("UTC"))).getTime());
-        System.out.println(Timestamp.valueOf(LocalDateTime.now()).getTime());
-        System.out.println(Timestamp.valueOf(LocalDateTime.now(ZoneId.of("UTC"))).getNanos());
-        System.out.println(LocalDateTime.now());
-        System.out.println(LocalDateTime.now().toLocalTime());
+        System.out.println("UTC Timestamp=" + Timestamp.valueOf(LocalDateTime.now(ZoneId.of("UTC"))).getTime());
+        System.out.println("Local Timestamp=" + Timestamp.valueOf(LocalDateTime.now()).getTime());
+        System.out.println("UTC Timestamp name=" + Timestamp.valueOf(LocalDateTime.now(ZoneId.of("UTC"))).getNanos());
+
+        System.out.println("GMT=" + LocalDateTime.now(ZoneId.of("GMT")));
+        System.out.println("UTC=" + LocalDateTime.now(ZoneId.of("UTC")));
+
+        System.out.println("Local DateTime=" + LocalDateTime.now());
+        System.out.println("Local Date=" + LocalDateTime.now().toLocalDate());
+        System.out.println("Local Time=" + LocalDateTime.now().toLocalTime());
     }
 }
