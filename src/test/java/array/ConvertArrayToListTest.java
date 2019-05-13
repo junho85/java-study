170187d4d9c79251cd00cf1ceee4c1dfabf60295
june.kim <june.kim@kakaocorp.com>
2019-05-13 13:51:22 +0900

    convert array to list

---
+++ src/test/java/array/ConvertArrayToListTest.java
@@ -0,0 +1,58 @@
+package array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConvertArrayToListTest {
+    @Test
+    public void test1() {
+        String[] array = {"1", "2", "3", "4"};
+        List<String> strings = Arrays.asList(array);
+
+        System.out.println(strings);
+    }
+
+    @Test
+    public void test2() {
+        String[] array = {"1", "2", "3", "4"};
+        List<String> strings = new ArrayList<>(Arrays.asList(array));
+
+        System.out.println(strings);
+    }
+
+    @Test
+    public void test3() {
+        String[] array = {"1", "2", "3", "4"};
+        List<String> strings = new ArrayList<>();
+
+        Collections.addAll(strings, array);
+
+        System.out.println(strings);
+    }
+
+    @Test
+    public void test4() {
+        List<String> strings = new ArrayList<String>() {{
+            add("1");
+        }};
+
+        String[] array = {"2", "3", "4"};
+
+        Collections.addAll(strings, array);
+
+        System.out.println(strings);
+    }
+
+    @Test
+    public void test5() {
+        String[] array = {"1", "2", "3", "4"};
+        List<String> strings = Arrays.stream(array).collect(Collectors.toList());
+
+        System.out.println(strings);
+    }
+}
