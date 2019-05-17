256a546b837aacfeca9b21aacaf6162816265089
june.kim <june.kim@kakaocorp.com>
2019-05-17 18:32:29 +0900

    convert collection to array

---
+++ src/test/java/array/ConvertCollectionToArray.java
@@ -0,0 +1,86 @@
+package array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ConvertCollectionToArray {
+    private Collection<String> getTestCollection() {
+        Collection<String> stringCollection = new ArrayList();
+        stringCollection.add("1");
+        stringCollection.add("2");
+        stringCollection.add("3");
+
+        return stringCollection;
+    }
+    @Test
+    public void test_collection_to_array() {
+        Collection<String> stringCollection = getTestCollection();
+
+        for (String s : stringCollection) {
+            System.out.println(s);
+        }
+
+        // convert
+        String[] stringArray = stringCollection.toArray(new String[stringCollection.size()]);
+        for (String s : stringArray) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void test_collection_to_array_shorter() {
+        Collection<String> stringCollection = getTestCollection();
+
+        for (String s : stringCollection) {
+            System.out.println(s);
+        }
+
+        // convert
+        String[] stringArray = stringCollection.toArray(new String[0]);
+
+        for (String s : stringArray) {
+            System.out.println(s);
+        }
+
+    }
+
+    @Test
+    public void test_collection_to_array_stream() {
+        Collection<String> stringCollection = getTestCollection();
+
+        for (String s : stringCollection) {
+            System.out.println(s);
+        }
+
+        // convert
+        String[] stringArray = stringCollection.stream()
+                .map(x -> new String(x))
+                .toArray(size -> new String[size]);
+
+        for (String s : stringArray) {
+            System.out.println(s);
+        }
+
+    }
+
+    @Test
+    public void test_collection_to_array_stream_shorter() {
+        Collection<String> stringCollection = getTestCollection();
+
+        for (String s : stringCollection) {
+            System.out.println(s);
+        }
+
+        // convert
+        String[] stringArray = stringCollection.stream()
+                .toArray(String[]::new);
+
+        for (String s : stringArray) {
+            System.out.println(s);
+        }
+
+    }
+
+}
