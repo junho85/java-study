e15fbda3abb76d128cf59757c8082d8b289a6e56
june.kim <june.kim@kakaocorp.com>
2019-05-31 20:16:26 +0900

    StringTest - null concatenation

---
+++ src/test/java/string/StringTest.java
@@ -0,0 +1,10 @@
+package string;
+
+import org.junit.Test;
+
+public class StringTest {
+    @Test
+    public void test() {
+        System.out.println("hello world " + null + " is null");
+    }
+}
