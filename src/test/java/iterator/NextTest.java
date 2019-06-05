b186fe51d96e13d41e4efaa21ed2223027384984
june.kim <june.kim@kakaocorp.com>
2019-06-05 16:39:49 +0900

    iterator - use hasNext

--- src/test/java/iterator/NextTest.java
+++ src/test/java/iterator/NextTest.java
@@ -8,41 +8,63 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class NextTest {
+    Iterator<String> getEmptyIterator() {
+        String[] array = {};
+        List<String> strings = Arrays.asList(array);
+
+        return strings.iterator();
+    }
+
+    Iterator<String> getNotEmptyIterator() {
+        String[] array = {"1","2","3"};
+        List<String> strings = Arrays.asList(array);
+
+        return strings.iterator();
+    }
+
     /**
      * next 가 없는데 next() 호출해서 예외 발생시킴
      */
     @Test
-    public void test() {
-        String[] array = {};
-        List<String> strings = Arrays.asList(array);
+    public void test_nosuchelementexception() {
+        Iterator<String> emptyIterator = getEmptyIterator();
 
-        System.out.println("test");
-        Iterator<String> iterator = strings.iterator();
-        System.out.println("test2");
-        System.out.println(iterator.next());
+        System.out.println(emptyIterator.next());
         // java.util.NoSuchElementException - RuntimeException
-        System.out.println("test3"); // not executed
+
+        System.out.println("should not not executed"); // not executed
     }
 
     /**
      * catch NoSuchElementException 을 잡아 봄
      */
     @Test
-    public void test2() {
-        String[] array = {};
-        List<String> strings = Arrays.asList(array);
-
-        System.out.println("test");
-        Iterator<String> iterator = strings.iterator();
-        System.out.println("test2");
+    public void test_try_catch() {
+        Iterator<String> emptyIterator = getEmptyIterator();
         try {
             // java.util.NoSuchElementException - RuntimeException
-            System.out.println(iterator.next());
+            System.out.println(emptyIterator.next());
         } catch (NoSuchElementException e) {
             System.out.println(e.getMessage()); // null
             System.out.println(e.getCause()); // null
         }
-        System.out.println("test3"); // not executed
+        System.out.println("executed");
+    }
+
+    @Test
+    public void test_safe() {
+        Iterator<String> emptyIterator = getEmptyIterator();
+        while (emptyIterator.hasNext()) {
+            String str = emptyIterator.next();
+            System.out.println(str);
+        }
+
+        // 1,2,3
+        Iterator<String> notEmptyIterator = getNotEmptyIterator();
+        while (notEmptyIterator.hasNext()) {
+            String str = notEmptyIterator.next();
+            System.out.println(str);
+        }
 
     }
 }
