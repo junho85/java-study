5dc57305f6b409752c3bb2e56e856d7cd0148d46
june.kim <june.kim@kakaocorp.com>
2019-06-05 16:33:56 +0900

    add - to check immutable or not

--- src/test/java/iterator/NextTest.java
+++ src/test/java/iterator/NextTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class NextTest {
+    /**
+     * next 가 없는데 next() 호출해서 예외 발생시킴
+     */
     @Test
     public void test() {
         String[] array = {};
@@ -18,5 +22,27 @@ public class NextTest {
         System.out.println(iterator.next());
         // java.util.NoSuchElementException - RuntimeException
         System.out.println("test3"); // not executed
+    }
+
+    /**
+     * catch NoSuchElementException 을 잡아 봄
+     */
+    @Test
+    public void test2() {
+        String[] array = {};
+        List<String> strings = Arrays.asList(array);
+
+        System.out.println("test");
+        Iterator<String> iterator = strings.iterator();
+        System.out.println("test2");
+        try {
+            // java.util.NoSuchElementException - RuntimeException
+            System.out.println(iterator.next());
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage()); // null
+            System.out.println(e.getCause()); // null
+        }
+        System.out.println("test3"); // not executed
+
     }
 }
