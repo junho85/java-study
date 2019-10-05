d05708667497d33324216b2e3a063c35e93a1c9f
junho85 <junho85@gmail.com>
2019-10-05 10:47:24 +0900

    Condition Test

---
+++ src/test/java/ConditionTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class ConditionTest {
+    @Test
+    public void test() {
+        System.out.println("hello");
+
+//        String type = "M"; // fail
+        String type = "K"; // fail
+//        String type = "C"; // success
+
+        if (!(type.equals("K") || type.equals("M"))) {
+            System.out.println("success...");
+        } else {
+            System.out.println("fail...");
+        }
+    }
+}
