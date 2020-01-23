a827e1b77114d06b2ee059e9a86120f6509841f4
junho85 <junho85@gmail.com>
2020-01-23 14:54:06 +0900

    string test

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -15,4 +15,22 @@ public class StringTest {
         System.out.println(str);
         System.out.println(str.replaceAll("\\n", "\\\\n"));
     }
+
+    @Test
+    public void test3() {
+        System.out.println("aaa" == "aa" + "a"); // true
+
+        String a = "aaa";
+        String b = "aaa";
+        System.out.println(a == b); // true
+
+        String aa = new String("aaa");
+        String bb = new String("aaa");
+        System.out.println(aa == bb); // false
+    }
+
+    public static void main(String[] args) {
+        String a = "aaa";
+        String b = "aaa";
+    }
 }
