333d60b18f487579e11a11a78cb44eb73bf64ac5
june.kim <june.kim@kakaocorp.com>
2020-03-20 23:22:11 +0900

    HashMap loop

--- src/main/java/hashmap/HashMapTest.java
+++ src/main/java/hashmap/HashMapTest.java
@@ -1,6 +1,8 @@
 package hashmap;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map.Entry;
 
 public class HashMapTest {
@@ -11,9 +13,27 @@ public class HashMapTest {
         map.put("hello", 22);
         map.put("world", 33);
 
-        for (Entry entry : map.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
+        System.out.println("=== keySet ===");
+        for (String key : map.keySet()) {
+            System.out.println(key + ":" + map.get(key));
         }
+
+        System.out.println("=== entrySet ===");
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        System.out.println("=== iterator ===");
+        Iterator<Entry<String, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Entry<String, Integer> entry = iterator.next();
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        System.out.println("=== forEach ===");
+        map.forEach((key, value) -> System.out.println(key + ":" + value));
+
+        System.out.println("=== stream forEach ===");
+        map.entrySet().stream().forEach(entry -> System.out.println(entry.getKey() + ":" + entry.getValue()));
     }
 }
--- src/main/java/hashset/HashSetTest.java
+++ src/main/java/hashset/HashSetTest.java
@@ -1,4 +1,4 @@
-package set;
+package hashset;
 
 import java.util.HashSet;
 import java.util.Set;
