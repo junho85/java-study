6cc8695eb3ac2f5a3520b3841e9d1e6b6c18ecc4
june.kim <junho85@gmail.com>
2020-10-19 13:29:11 +0900

    dateToLocalDate, localDateToZonedDateTime

---
+++ src/test/java/date/LocalDateTest.java
@@ -0,0 +1,31 @@
+package date;
+
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+public class LocalDateTest {
+    @Test
+    public void dateToLocalDate() throws ParseException {
+        // Date to LocalDate
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date parse = sdf.parse("2020-10-19");
+        LocalDate localDate = parse.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        // LocalDate to ZonedDateTime
+        ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZoneId.of("Asia/Seoul"));
+        System.out.println(zonedDateTime); // 2020-10-19T00:00+09:00[Asia/Seoul]
+    }
+
+    @Test
+    public void localDateToZonedDateTime() {
+        LocalDate localDate = LocalDate.parse("2020-10-19");
+        ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZoneId.of("Asia/Seoul"));
+        System.out.println(zonedDateTime); // 2020-10-19T00:00+09:00[Asia/Seoul]
+    }
+}
