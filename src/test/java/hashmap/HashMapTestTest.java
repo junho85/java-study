da16343f2a24abd60c94f8c59458432d92cbd36a
june.kim <junho85@gmail.com>
2020-11-07 23:44:16 +0900

    HashMapTest

--- src/test/java/hashmap/HashMapTestTest.java
+++ src/test/java/hashmap/HashMapTestTest.java
@@ -2,12 +2,37 @@ package hashmap;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HashMapTestTest {
+    
+    /**
+     * java9 Map.of
+     */
+    @Test
+    void test() {
+        Map<String, Object> test = Map.of(
+                "name", "Kim",
+                "age", 36
+        );
+        System.out.println(test);
+    }
+
+    /**
+     * java9 Map.ofEntries
+     */
     @Test
     void test2() {
-        System.out.println("hello");
+        Map<String, Object> test = Map.ofEntries(
+                new AbstractMap.SimpleEntry<>("name", "Kim"),
+                new AbstractMap.SimpleEntry<>("age", 36)
+        );
+        System.out.println(test);
     }
 
 }
