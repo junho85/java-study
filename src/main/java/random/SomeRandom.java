67d2c93a1b6dc73ccafd491c03434effddf6b902
june.kim <junho85@gmail.com>
2021-04-27 22:00:54 +0900

    Random number test

---
+++ src/main/java/random/SomeRandom.java
@@ -0,0 +1,4 @@
+package random;
+
+public class SomeRandom {
+}
---
+++ src/test/java/random/SomeRandomTest.java
@@ -0,0 +1,21 @@
+package random;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.Random;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeRandomTest {
+
+    @RepeatedTest(100)
+    void test() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(10);
+
+        then(randomNumber)
+                .as("check that random number is less than 10")
+                .isLessThan(10);
+    }
+}
